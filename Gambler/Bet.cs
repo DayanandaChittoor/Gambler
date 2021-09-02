aeb7937da7c5f075bf48f73fa0524488bf66429b
DayanandaChittoor <seenu.daya5@gmail.com>
2021-09-02 11:25:06 +0530

    Add project files.

---
+++ Gambler/Bet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    class Bet
+    {
+    }
+}
---
+++ Gambler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Gambler Program");
+
+            int stake = 100;
+            int bet = 1;
+
+
+
+        }
+    }
+}
